6ee12fb5ad54115b30062cc7122cda4ecb14904b
Marco Piraccini <marco.piraccini@gmail.com>
2017-03-26 18:53:14 +0200

    added more structure

--- .labrc.js
+++ .labrc.js
@@ -12,7 +12,7 @@ module.exports = {
   globals: '__core-js_shared__', // came from power-assert
   lint: true,
   timeout: 35e3,
-  threshold: 80,
+  threshold: 70,
   transform: './node_modules/lab-espower-transformer',
   verbose: true,
   reporter: outputs.map(o => o.reporter),
