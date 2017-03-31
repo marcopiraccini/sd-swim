f6cf72cf9cc46ccc31ea7b4c58e84c5ec1fb5ef1
Marco Piraccini <marco.piraccini@gmail.com>
2017-03-31 07:10:18 +0200

    join messagesa ans send tests

--- test/client.js
+++ test/client.js
@@ -1,17 +1,61 @@
 /* eslint no-console:0 */
 
-const Client = require('../lib/client')
 const assert = require('power-assert')
 const Lab = require('lab')
 const lab = exports.lab = Lab.script()
 const pino = require('pino')
-const {describe, it} = lab
+const proxyquire = require('proxyquire')
+const {describe, it, before, beforeEach} = lab
+const Messages = require('../lib/messages')
 
-describe('Communication Client', () => {
-  it('should test communication', done => {
-    const client = new Client({logger: pino()})
-    // console.log(net)
-    assert.ok(client)
+describe('Protocol Client', () => {
+
+  let client
+  let sentMessages = []
+  const opts = {}
+  const messages = new Messages(opts)
+
+  before(done => {
+    const mockedClient = {
+      send: (message, offset, length, port, address, cb) => {
+        sentMessages.push({message, offset, length, port, address})
+        cb()
+      },
+      close: () => {}
+    }
+    const mockedDgram = {
+      createSocket: () => (mockedClient)
+    }
+    const Client = proxyquire('../lib/client', {
+      'dgram' : mockedDgram
+    })
+    client = new Client({logger: pino()})
+    done()
+  })
+
+  beforeEach(done => {
+    sentMessages = []
+    done()
+  })
+
+  it('should send a bunch of join messages', done => {
+    const host1 = {host: 'host1', port: 1234}
+    const host2 = {host: 'host2', port: 5678}
+    const hosts = [host1, host2]
+    const joinMessages = messages.createJoinMessages(hosts)
+    client.sendMessages(joinMessages, '127.0.0.1', 6666)
+    assert.equal(sentMessages.length, 2)
+    const firstJoin = messages.decodeMessage(sentMessages[0].message)
+    const secondJoin = messages.decodeMessage(sentMessages[1].message)
+
+    assert.deepEqual(firstJoin.target, host1)
+    assert.ok(firstJoin.token)
+    assert.equal(firstJoin.type, 0)
+
+    assert.deepEqual(secondJoin.target, host2)
+    assert.ok(secondJoin.token)
+    assert.equal(secondJoin.type, 0)
+
     done()
   })
 })
--- test/messages.js
+++ test/messages.js
@@ -1,17 +1,14 @@
 /* eslint no-console:0 */
 
-const protobuf = require('protocol-buffers')
-const fs = require('fs')
 const Lab = require('lab')
 const lab = exports.lab = Lab.script()
 const assert = require('power-assert')
 const Messages = require('../lib/messages')
 const {describe, it} = lab
 
-describe('TEST NET COMM - TODO', () => {
+describe('Messages', () => {
 
-  const {Msg: {decode}} = protobuf(fs.readFileSync('./lib/proto/sd-swim.proto'))
-  const messages = new Messages({})
+  const messages = new Messages()
 
   it('should create a join message correctly', done => {
     const host1 = {host: 'host1', port: 1234}
@@ -20,8 +17,8 @@ describe('TEST NET COMM - TODO', () => {
 
     const joinMessages = messages.createJoinMessages(hosts)
     assert.equal(joinMessages.length, 2)
-    decode(joinMessages[0])
-    decode(joinMessages[1])
+    messages.decodeMessage(joinMessages[0])
+    messages.decodeMessage(joinMessages[1])
     done()
   })
 
