240cc8adc7dd92e68c4b887dcd22d5fbb90fc7e9
Marco Piraccini <marco.piraccini@gmail.com>
2017-05-04 07:25:44 +0200

    multiple nodes startup tests. Fix on closing

--- test/common.js
+++ test/common.js
@@ -1,21 +1,21 @@
 const {concatSeries} = require('async')
 const SDSwim = require('../lib/sd-swim')
 
-exports.startNode = (port, cb) => {
-  const target = new SDSwim({port: port})
-  target.start(() => {
-    cb(null, target)
+exports.startNode = (opts, cb) => {
+  const target = new SDSwim(opts)
+  target.start(err => {
+    cb(err, target)
   })
 }
 
 exports.stopNode = (node, cb) => {
-  node.stop(() => {
-    cb()
+  node.stop(err => {
+    cb(err)
   })
 }
 
 // Start nodes on the same host, using the ports array
-exports.startNodes = (ports, cb) => concatSeries(ports, exports.startNode, cb)
+exports.startNodes = (opts, cb) => concatSeries(opts, exports.startNode, cb)
 
 // Start nodes on the same host, using the ports array
 exports.stopNodes = (nodes, cb) => concatSeries(nodes, exports.stopNode, cb)
--- test/failure.js
+++ test/failure.js
@@ -15,13 +15,12 @@ describe('Failure Detector', () => {
   describe('given a started node', () =>  {
 
     let target
-    const ports = [12340]
+    const nodeOpts = [{port: 12340}]
 
-    beforeEach(done => startNodes(ports, function(err, results) {
-        [target] = results
-        done()
-      })
-    )
+    beforeEach(done => startNodes(nodeOpts, function(err, results) {
+      [target] = results
+      done()
+    }))
 
     afterEach(done => stopNodes([target], done))
 
@@ -37,14 +36,14 @@ describe('Failure Detector', () => {
       })
 
       sdswim.on('ping', target => {
-         assert.strictEqual(target.port, ports[0])
+         assert.strictEqual(target.port, nodeOpts[0].port)
          sdswim.stop(() => {
            done()
          })
       })
 
       sdswim.on('ack', target => {
-         assert.strictEqual(target.node.port, ports[0])
+         assert.strictEqual(target.port, nodeOpts[0].port)
          sdswim.stop(() => {
            done()
          })
@@ -53,5 +52,50 @@ describe('Failure Detector', () => {
       sdswim.start()
     })
 
+  })
+
+  describe('given 5 started nodes', () =>  {
+
+    let nodes
+    const opts =
+      [{port: 12340},
+       {port: 12341, hosts: [{host: '127.0.0.1', port:12340}]},
+       {port: 12342, hosts: [{host: '127.0.0.1', port:12340}]},
+       {port: 12343, hosts: [{host: '127.0.0.1', port:12340}]},
+       {port: 12344, hosts: [{host: '127.0.0.1', port:12340}]},
+       {port: 12345, hosts: [{host: '127.0.0.1', port:12340}]}]
+
+    beforeEach(done => startNodes(opts, function(err, res) {
+      nodes = res
+      done(err)
+    }))
+
+    afterEach(done => stopNodes(nodes, done))
+
+    it('should start 5 nodes, and then stop them and the member list must be coherent', done => {
+
+      const expectedMemberListAfterStart =
+      [ { host: '127.0.0.1', port: 12340 },
+        { host: '127.0.0.1', port: 12341 },
+        { host: '127.0.0.1', port: 12342 },
+        { host: '127.0.0.1', port: 12343 },
+        { host: '127.0.0.1', port: 12344 },
+        { host: '127.0.0.1', port: 12345 } ]
+
+      // check the member list after 2 secs
+      setTimeout(() => {
+        assert.deepEqual(nodes[0].memberList, expectedMemberListAfterStart)
+        assert.deepEqual(nodes[1].memberList, expectedMemberListAfterStart)
+        assert.deepEqual(nodes[2].memberList, expectedMemberListAfterStart)
+        assert.deepEqual(nodes[3].memberList, expectedMemberListAfterStart)
+        assert.deepEqual(nodes[4].memberList, expectedMemberListAfterStart)
+        assert.deepEqual(nodes[5].memberList, expectedMemberListAfterStart)
+
+        // TODO: stop and check the member list after every stop
+
+        done()
+      }, 1000)
+
+    })
   })
 })
